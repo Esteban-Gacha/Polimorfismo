c2c0bcd0d5d709b761d907288b07c36c9d5d3813
Esteban-Gacha <159479229+Esteban-Gacha@users.noreply.github.com>
2024-10-22 17:42:04 -0500

    Add files via upload

---
+++ Polimorfismo/src/main/java/co/edu/uniquindio/poo/Cliente.java
@@ -0,0 +1,30 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Cliente extends Persona {
+
+    
+    private Collection <Reserva> reservas;
+
+    public Cliente(String nombre, String apellido, String cedula, int edad){
+        super(nombre, apellido, cedula, edad);
+        reservas= new LinkedList<>();
+    }
+
+    public Collection <Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(Collection <Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [reservas=" + reservas + "]";
+    }
+    
+    
+}
